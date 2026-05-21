83c39d25b8f550aa3f4bcd8f7eef7e064a90da96
Computer Lab 4 <cl4@uit.edu>
2026-05-21 10:19:41 +0500

    first code

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI Pipeline 🚀");
+        System.out.println("Build is running successfully!");
+        
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
